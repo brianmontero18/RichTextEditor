0fd06959e5d2634e050387151ec123572cacf784
brianmontero18 <brianmontero18@gmail.com>
2019-10-03 21:46:27 -0300

    Update Editor.js

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,27 +1,73 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import ControlPanel from "./control-panel/ControlPanel";
 import FileZone from "./file-zone/FileZone";
+import * as Commands from '../commands';
 
-const Editor = () => {
-    const [state, setState] = useState({ selected: '' });
+const setPrivateProps = (defaultProps) => defaultProps.map((props) => ({
+    ...props,
+    isActive: false
+}));
 
-    // const handleChange = (event) => {
-    //     setState({ selected: event.target.innerHTML })
-    // };
+const getParentsNode = (node, target) => {
+    const fileZoneNode = document.querySelector('#file');
+    return (node.parentElement && !node.parentElement.isEqualNode(fileZoneNode) ? getParentsNode(node.parentElement, target) : []).concat([node]);
+};
 
-    const getSelection = ({ target }) => {
-        if(target.innerHTML) {
-            const node = window.getSelection().getRangeAt(0).startContainer;
-            const parents = (node) => (node.parentElement ? parents(node.parentElement) : []).concat([node]);
-            console.log(parents(node));
-            setState({ selected: target.outerHTML })
+const getFormat = (target, state) => {
+    const node = window.getSelection().getRangeAt(0).startContainer;
+    const parents = getParentsNode(node, target);
+    
+    if(parents.length > 0) {
+        return state.map((button) => ({
+            ...button,
+            isActive: parents.some((optionMenu) => optionMenu.tagName === Commands[button.command].tagName)
+        }));
+    }
+};
+
+const setBasicFormat = (command, state) => {
+    document.execCommand(command, false);
+    return state.map((button) => {
+        if(button.command === command) { 
+            return {
+                ...button,
+                isActive: !button.isActive
+            };
+        } else {
+            return button;
         }
-    };
+    });
+};
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'setFormat': {
+            if(Commands.bold.name === action.payload.command || Commands.bold.name === action.payload.command || Commands.bold.name === action.payload.command) {
+                return setBasicFormat(action.payload.command, state);
+            } else {
+                return null;
+            }
+            break;
+        }
+        case 'getFormat':
+            return getFormat(action.payload, state);
+        default:
+            throw new Error();
+    }
+};
+
+const Editor = ({ formatActions }) => {
+    const [formatButtons, dispatch] = useReducer(reducer, setPrivateProps(formatActions));
+    
     return (
         <React.Fragment>
-            <ControlPanel selected={state.selected} />
-            <FileZone getSelection={getSelection} />
+            <ControlPanel
+                formatButtons={formatButtons}
+                onClick={(command) => dispatch({ type: 'setFormat', payload: { command } })}
+            />
+            <FileZone
+                onDoubleClick={({ target }) => dispatch({ type: 'getFormat', payload: { target } })}
+            />
         </React.Fragment>
     );
 };
