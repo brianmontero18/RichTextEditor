c0b0577aa2fc1587e80cee93f272d73f394aae55
brianmontero18 <brianmontero18@gmail.com>
2019-10-03 21:54:14 -0300

    Create commands.js

---
+++ src/commands.js
@@ -0,0 +1,12 @@
+export const bold = {
+    name: 'bold',
+    tagName: 'B' 
+};
+export const italic = {
+    name: 'italic',
+    tagName: 'I' 
+};
+export const underline = {
+    name: 'underline',
+    tagName: 'U' 
+};
