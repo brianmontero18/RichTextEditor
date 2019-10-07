4795b3ef86a22c6e542af7813ecd3367f0e15048
brianmontero18 <brianmontero18@gmail.com>
2019-10-07 17:25:29 -0300

    Create index.js

---
+++ src/Editor/commands/index.js
@@ -0,0 +1,17 @@
+import * as Types from './types';
+
+export const bold = {
+    name: 'bold',
+    tagName: 'B',
+    type: Types.BASIC 
+};
+export const italic = {
+    name: 'italic',
+    tagName: 'I',
+    type: Types.BASIC
+};
+export const underline = {
+    name: 'underline',
+    tagName: 'U',
+    type: Types.BASIC 
+};
